bfe801821cfc6950d2150486b9c7b47828467c65
Wojciech Tyziniec <wojtek@netcamp.pl>
2015-05-19 19:34:37 +0200

    ASAnnotation class uses ASPOI

--- AugmentedSzczecin/AugmentedSzczecin/ASAnnotation.swift
+++ AugmentedSzczecin/AugmentedSzczecin/ASAnnotation.swift
@@ -2,20 +2,23 @@
 //  ASAnnotation.swift
 //  AugmentedSzczecin
 //
-//  Created by Patronage on 11.04.2015.
+//  Created by Wojciech Tyziniec on 10.05.2015.
 //  Copyright (c) 2015 BLStream. All rights reserved.
 //
 
 import UIKit
 
 class ASAnnotation: NSObject, BLSAugmentedAnnotation {
-   
-    let type: String
-    let coordinate: CLLocationCoordinate2D
     
-    init(type: String, withCoordinate coordinate: CLLocationCoordinate2D) {
-        self.type = type
-        self.coordinate = coordinate
+    var poi: ASPOI
+    var coordinate: CLLocationCoordinate2D
+    var title: String
+    var subtitle: String
+    
+    init(poi: ASPOI, id: Int, coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.poi = poi
+        self.title = self.poi.name!
+        self.subtitle = self.poi.tag!
+        self.coordinate = CLLocationCoordinate2DMake(self.poi.latitude as! Double, self.poi.longitude as! Double)
     }
-    
 }
