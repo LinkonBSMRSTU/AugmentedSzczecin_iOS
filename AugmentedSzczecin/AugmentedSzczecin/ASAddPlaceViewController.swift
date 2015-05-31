684b73d6133e969de56965acea04ea15b3374b19
Grzegorz Pawlowicz <grzegorz.pawlowicz@me.com>
2015-05-31 14:04:36 +0200

    AS-423 add ASAddPoiViewController + VC in storyboard

---
+++ AugmentedSzczecin/AugmentedSzczecin/ASAddPlaceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ASAddPlaceViewController.swift
+//  AugmentedSzczecin
+//
+//  Created by Grzegorz Pawlowicz on 31.05.2015.
+//  Copyright (c) 2015 BLStream. All rights reserved.
+//
+
+import UIKit
+
+class ASAddPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
