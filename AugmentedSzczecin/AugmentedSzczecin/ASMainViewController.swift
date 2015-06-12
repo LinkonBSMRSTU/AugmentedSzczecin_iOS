45204f2aeff7dbf8ce494bc1dd864f1231b8435e
Kacper Tłusty <kacper.tlusty@outlook.com>
2015-06-12 17:42:59 +0200

    Improve menu UI

--- AugmentedSzczecin/AugmentedSzczecin/ASMainViewController.swift
+++ AugmentedSzczecin/AugmentedSzczecin/ASMainViewController.swift
@@ -15,6 +15,36 @@ class ASMainViewController: UIViewController {
     @IBOutlet weak var aboutButton: UIButton!
     @IBOutlet weak var showAddPOI: UIButton!
     
+    override func viewDidLoad() {
+        
+        searchButton.backgroundColor = UIColor.dodgerBlueAugmentedColor()
+        searchButton.tintColor = UIColor.whiteAugmentedColor()
+        searchButton.layer.cornerRadius = 50
+        searchButton.layer.borderWidth = 5
+        searchButton.layer.borderColor = UIColor.dodgerBlueAugmentedColor().CGColor
+        
+        showMapButton.backgroundColor = UIColor.clearColor()
+        showMapButton.tintColor = UIColor.redAugmentedColor()
+        showMapButton.layer.cornerRadius = 50
+        showMapButton.layer.borderWidth = 5
+        showMapButton.layer.borderColor = UIColor.redAugmentedColor().CGColor
+
+        aboutButton.backgroundColor = UIColor.clearColor()
+        aboutButton.tintColor = UIColor.grayAugmentedColor()
+        aboutButton.layer.cornerRadius = 50
+        aboutButton.layer.borderWidth = 5
+        aboutButton.layer.borderColor = UIColor.grayAugmentedColor().CGColor
+        
+        showAddPOI.backgroundColor = UIColor.clearColor()
+        showAddPOI.tintColor = UIColor.blackAugmentedColor()
+        showAddPOI.layer.cornerRadius = 50
+        showAddPOI.layer.borderWidth = 5
+        showAddPOI.layer.borderColor = UIColor.blackAugmentedColor().CGColor
+        
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: Selector("switchColor"), userInfo: nil, repeats: true)
+    }
+    
     @IBAction func searchButtonTap(sender: AnyObject) {
         self.performSegueWithIdentifier("searchSegue", sender: nil)
     }
@@ -31,4 +61,24 @@ class ASMainViewController: UIViewController {
         self.performSegueWithIdentifier("showAddPOISegue", sender: nil)
     }
     
+    func switchColor() {
+        let buttons = [showMapButton, searchButton, aboutButton, showAddPOI]
+        for index in 0...3 {
+            if (buttons[index].backgroundColor != UIColor.clearColor()) {
+                buttons[index].backgroundColor = UIColor.clearColor()
+                buttons[index].tintColor = UIColor(CGColor: buttons[index].layer.borderColor)
+                
+                if (index == buttons.count-1) {
+                    buttons.first?.backgroundColor = UIColor(CGColor: buttons.first!.layer.borderColor)
+                    buttons.first?.tintColor = UIColor.whiteAugmentedColor()
+                } else {
+                    buttons[index+1].backgroundColor = UIColor(CGColor: buttons[index+1].layer.borderColor)
+                    buttons[index+1].tintColor = UIColor.whiteAugmentedColor()
+                    
+                }
+                break
+            }
+        }
+    }
 }
+
